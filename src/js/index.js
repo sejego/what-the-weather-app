867e167eb84bccceccbe9c3413bc26f2dc847a7b
sejego <sergei.jegorov@protonmail.com>
2023-12-01 18:36:00 +0100

    restore enter func

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,15 @@
-import "../styles/style.css"
-import "../styles/error.css"
+import "../styles/style.css";
+import "../styles/error.css";
 
 import { updateWeather } from "./fetch_weather_data";
-import { isSearchFieldEmpty} from "./utils";
+import { isSearchFieldEmpty } from "./utils";
 
 const search = document.getElementsByName("Location")[0];
 const searchBtn = document.getElementById("search-btn");
 
 searchBtn.addEventListener("click", (event) => processWeatherRequest(event));
+
+search.addEventListener("keypress", (event) => processEnterPress(event));
 
 async function processWeatherRequest(event) {
   /* prevent double click event from happening */
@@ -19,5 +21,10 @@ async function processWeatherRequest(event) {
   search.value = "";
 }
 
+function processEnterPress(event) {
+  if (event.key === "Enter") {
+    searchBtn.click();
+  }
+}
 /* Initial update with default location */
 updateWeather();
